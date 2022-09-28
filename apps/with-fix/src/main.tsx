73988a4c1e259fba22d9acb35f27bf0032742fd8
Richard Evans <richard.evans@meraki.net>
2022-09-28 12:27:18 -0400

    setup

---
+++ apps/with-fix/src/main.tsx
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+import { TestImage } from '@android-issue/imagelib';
+
+function App() {
+  return <TestImage />;
+}
+
+AppRegistry.registerComponent('WithFix', () => App);
---
+++ libs/imagelib/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+export function TestImage() {
+  return <Image source={require('./ic_launcher.png')} />;
+}
